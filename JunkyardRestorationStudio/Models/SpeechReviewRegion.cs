65d56889de484d1a923e80bcf0853088fa39f339
Josh Sharp <jsharp9009@live.com>
2026-09-10 18:16:54 -0500

    Map generated snake case fields

--- JunkyardRestorationStudio/Models/SpeechReviewRegion.cs
+++ JunkyardRestorationStudio/Models/SpeechReviewRegion.cs
@@ -1,3 +1,5 @@
+using System.Text.Json.Serialization;
+
 namespace JunkyardRestorationStudio.Models;
 
 public class SpeechReviewRegion
@@ -5,13 +7,24 @@ public class SpeechReviewRegion
     public int Id { get; set; }
     public double Start { get; set; }
     public double End { get; set; }
+
+    [JsonPropertyName("subtitle_ids")]
     public int[] SubtitleIds { get; set; } = [];
+
     public string Text { get; set; } = "";
+
+    [JsonPropertyName("boundary_start_source")]
     public string BoundaryStartSource { get; set; } = "";
+
+    [JsonPropertyName("boundary_end_source")]
     public string BoundaryEndSource { get; set; } = "";
+
     public string Quality { get; set; } = "";
     public string Speaker { get; set; } = "";
+
+    [JsonPropertyName("speaker_confidence")]
     public string SpeakerConfidence { get; set; } = "";
+
     public string Notes { get; set; } = "";
 }
 
